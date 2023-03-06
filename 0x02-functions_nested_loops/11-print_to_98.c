cb8bee175e751f9c6db65e803dbb25bd3150fccf
maryouwm <maryum1425@gmail.com>
2023-03-06 03:24:56 -0800

    0x07-pointers_arrays_strings

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,7 +7,7 @@
  * @n: print from this number
  */
 
-void print_to_98(int n)
+int print_to_98(int n)
 {
 	int i, j;
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef main_h
+#define main_h
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +14,5 @@ void times_tablet(void);
 int add(int, int);
 int print_to_98(int n);
 void print_times_tablet(int n);
+
+#endif
